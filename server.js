68654fc7dcc3a6de4383cd8daf853d7151bf0317
bz-888 <benjamin.zheng95@gmail.com>
2023-10-31 08:22:58 -0500

    added started code, need to go back and add code from yesterday

--- server.js
+++ server.js
@@ -1,47 +1,61 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
+// load the env consts
 require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+// session middleware
+const session = require('express-session');
+const passport = require('passport');
+const methodOverride = require('method-override');
+const indexRoutes = require('./routes/index');
+
+
+// create the Express app
+const app = express();
+
+// connect to the MongoDB with mongoose
 require('./config/database');
+// configure Passport
+require('./config/passport');
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const gamesRouter = require("./routes/games");
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// mount the session middleware
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/games", gamesRouter);
+app.use(passport.initialize());
+app.use(passport.session());
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+// Add this middleware BELOW passport middleware
+app.use(function (req, res, next) {
+  res.locals.user = req.user; // assinging a property to res.locals, makes that said property (user) availiable in every
+  // single ejs view
+  next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// mount all routes with appropriate base paths
+app.use('/', indexRoutes);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+// invalid request, send 404 page
+app.use(function (req, res) {
+  res.status(404).send('Cant find that!');
 });
 
 module.exports = app;
